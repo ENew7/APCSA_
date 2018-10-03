bb3d96cfe8c51eb02b97e7c5083c184a11ca96d1
ENew7 <42822049+ENew7@users.noreply.github.com>
2018-10-03 10:20:41 -0700

    sgdhfj

--- Projects/Project 2 - Ball World/BallRunner.java
+++ Projects/Project 2 - Ball World/BallRunner.java
@@ -10,7 +10,8 @@ public class BallRunner
     public static void run(){
         BallWorld ballWorld = new BallWorld(200, 200);
         TGPoint startPoint = new TGPoint(0, 0);
-        BallBot ballBot = new BallBot(ballWorld, startPoint, 0.0, 25);  
+        BallBot ballBot = new BallBot(ballWorld, startPoint, 0.0, 25);
+        //creats the ball
         while(true){
             if(ballBot.canMoveForward(ballWorld) == true){
                 ballBot.moveForward();
@@ -19,7 +20,9 @@ public class BallRunner
                 ballBot.setHeading(ballBot.getHeading()+90);
             }
         }
+        //moves the ball
     }
+    //^creates a ball at (0,0), has ball ride along the wall
     public int findFreeBallBotIndex(BallBot[] ballBotArray){
         int len = ballBotArray.length;
         for(int i = 0; i < len; i++){
@@ -29,6 +32,7 @@ public class BallRunner
         }
         return len;
     }
+    //finds open space in array
     public static void act2Runner(){
         BallWorld ballWorld = new BallWorld(750, 750);
         TGPoint tgpoint = new TGPoint(Math.random()*100, Math.random()*100);
@@ -40,6 +44,7 @@ public class BallRunner
                    BallBot ballBot = new BallBot(ballWorld, tgpoint, Math.random()*360, 25);
                    ballArray[i] = ballBot;
                 }
+                //makes ballBots
                 if(ballArray[i]!=null){
                     if(ballArray[i].canMoveForward(ballWorld) == true){
                         ballArray[i].moveForward();
@@ -48,6 +53,7 @@ public class BallRunner
                         ballArray[i].setHeading(ballArray[i].getHeading()+90);
                     }
                 }
+                //moves ballBots
             }
         }
         
@@ -55,10 +61,12 @@ public class BallRunner
     public double distanceBetweenPoints(TGPoint point1, TGPoint point2){
         return Math.sqrt(Math.pow(point1.x-point2.x,2.0)+(Math.pow(point1.y-point2.y,2.0)));
     }
+    //finds distance between points
     public boolean entranceClear (BallBot[] ballBotArray, TGPoint entrancePoint){
-        if(ballRunner.findFreeBallBotIndex(ballArray) < ballArray.length){
-            BallBot ballBot = new BallBot(ballWorld, tgpoint, Math.random()*360, 25);
-            ballArray[i] = ballBot;
+        for(int i = 0; i < ballBotArray.length; i++){
+            if(ballBotArray[i] != null){
+                
+            }
         }
     }
 }
