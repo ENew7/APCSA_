80512391232faaccb0a4664fe24292f205e8ac4c
ENew7 <42822049+ENew7@users.noreply.github.com>
2018-10-05 10:30:59 -0700

    rf

---
+++ Labs/SentenceBuilder_Lab1005/SentenceBuilder.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class SentenceBuilder here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SentenceBuilder
+{
+    public String[] nouns = {"Thomas the Tank Engine", "moth", "continent of Africa", "Danny DeVito", "puppy", "pizza", "tower", "boy", "girl", "attack helicopter", "dwarf"};
+    public String[] verbs = {"flies","jumps","dances","runs","sings","licks","eats","slides","climbs","kicks"};
+    public String[] adjectives = {"","","","","","","","","",""};
+    public String[] determiners = {"","","","","","","","","",""};
+    public String[] adverbs = {"","","","","","","","","",""};
+    public String[] propsitions = {"","","","","","","","","",""};
+    public void loadSentences(){
+        
+    }
+    public String getSentence(){
+        
+    }
+    //public String getNounPhrase()
+    //public String getPrepositionalPhrase()
+    //public String getVerbPhrase()
+    public String getRandomNoun(){
+        
+    }
+    //public String getRandomVerb()
+    //public String getRandomAdjective()
+    //public String getRandomAdverb()
+    //public String getRandomDeterminer()
+    //public String getRandomPreposition()
+
+}
--- Projects/Project 2 - Ball World/BallRunner.java
+++ Projects/Project 2 - Ball World/BallRunner.java
@@ -7,66 +7,82 @@
  */
 public class BallRunner
 {
-    public static void run(){
-        BallWorld ballWorld = new BallWorld(200, 200);
-        TGPoint startPoint = new TGPoint(0, 0);
-        BallBot ballBot = new BallBot(ballWorld, startPoint, 0.0, 25);
-        //creats the ball
-        while(true){
-            if(ballBot.canMoveForward(ballWorld) == true){
-                ballBot.moveForward();
-            }   
-            else{
-                ballBot.setHeading(ballBot.getHeading()+90);
-            }
-        }
-        //moves the ball
-    }
-    //^creates a ball at (0,0), has ball ride along the wall
-    public int findFreeBallBotIndex(BallBot[] ballBotArray){
-        int len = ballBotArray.length;
-        for(int i = 0; i < len; i++){
-            if(ballBotArray[i] == null){
-                len = 1;
-            }
-        }
-        return len;
-    }
-    //finds open space in array
-    public static void act2Runner(){
-        BallWorld ballWorld = new BallWorld(750, 750);
-        TGPoint tgpoint = new TGPoint(Math.random()*100, Math.random()*100);
-        BallBot[] ballArray = new BallBot[10];
-        BallRunner ballRunner = new BallRunner();
-        while(true){
-            for(int i = 0; i < ballArray.length; i++){
-                if(ballRunner.findFreeBallBotIndex(ballArray) < ballArray.length){
-                   BallBot ballBot = new BallBot(ballWorld, tgpoint, Math.random()*360, 25);
-                   ballArray[i] = ballBot;
-                }
-                //makes ballBots
-                if(ballArray[i]!=null){
-                    if(ballArray[i].canMoveForward(ballWorld) == true){
-                        ballArray[i].moveForward();
-                    }
-                    else{
-                        ballArray[i].setHeading(ballArray[i].getHeading()+90);
-                    }
-                }
-                //moves ballBots
-            }
-        }
-        
-    }
-    public double distanceBetweenPoints(TGPoint point1, TGPoint point2){
-        return Math.sqrt(Math.pow(point1.x-point2.x,2.0)+(Math.pow(point1.y-point2.y,2.0)));
-    }
-    //finds distance between points
-    public boolean entranceClear (BallBot[] ballBotArray, TGPoint entrancePoint){
+    //checks for null so it can create new balls
+    public static int findFreeBallBotIndex(BallBot[] ballBotArray){
+        int z = ballBotArray.length;
         for(int i = 0; i < ballBotArray.length; i++){
-            if(ballBotArray[i] != null){
-                
+            if (ballBotArray[i] == null){
+                z = i;
             }
         }
+        return z;
+    }
+    //finds the distance between balls
+    public static double distanceBetweenPoints(TGPoint point1, TGPoint point2){
+        return Math.sqrt(Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2));
+    }
+    //makes sure enterance is clar so balls dont fuse
+    public static boolean entranceClear (BallBot[] ballBotArray, TGPoint entrancePoint){
+        boolean t = true;
+        for(int i = 0; i < ballBotArray.length; i++){
+            if (ballBotArray[i] != null){
+                if(distanceBetweenPoints(ballBotArray[i].getPoint(), entrancePoint) < (2 * ballBotArray[i].getRadius())){
+                    t = false;
+                }
+            }
+        }
+        return t;
+    }
+    //creates the window, the balls, and makes them move
+    public static void ballWorldRunner(){
+        BallWorld ballWorld = new BallWorld(1200,800);
+        BallBot[] ballBotArray = new BallBot[10];
+        TGPoint startPoint = new TGPoint(0,0);
+        while(true){
+            //mkaes balls when enteracne is clear
+            if(findFreeBallBotIndex(ballBotArray) < ballBotArray.length){
+                if(entranceClear(ballBotArray, startPoint)){
+                    int balls = findFreeBallBotIndex(ballBotArray);
+                    ballBotArray[balls] = new BallBot(ballWorld, startPoint, (Math.random() * 360), 25);
+                }
+            }
+            for(int i = 0; i < ballBotArray.length; i++){
+                if(ballBotArray[i] != null){
+                    BallBot ball = ballBotArray[i];
+                    //balls bouncing around
+                    if(ball.canMoveForward(ballWorld)){
+                        if(ballBotToBounceOff(ballBotArray[i], ballBotArray) == null){
+                            ball.moveForward();
+                        }else{
+                            ball.setHeading((int)(Math.random()*360));
+                        }
+                    }else{
+                        ball.setHeading((int)(Math.random()*360));
+                    }
+                }
+            }
+        }
+    }
+    //bouncing when balls hit each other
+    public static BallBot ballBotToBounceOff(BallBot ballBot, BallBot[] ballBotArray){
+        TGPoint point = ballBot.getPoint();
+        TGPoint nextPoint = ballBot.forwardPoint();
+        BallBot ret = null;
+        boolean C1 = false;
+        boolean C2 = false;
+        for(int i = 0; i < ballBotArray.length; i++){
+            BallBot otherBallBot = ballBotArray[i];
+            //checking for impacts based on radii and where the balls will be
+            if(otherBallBot != null && otherBallBot != ballBot && ballBot != null){
+                double currentDistance = distanceBetweenPoints(ballBot.getPoint(), otherBallBot.getPoint());
+                if(currentDistance <= (ballBot.getRadius() + otherBallBot.getRadius())){
+                    double nextDistance = distanceBetweenPoints(nextPoint, otherBallBot.getPoint());
+                    if(nextDistance <= currentDistance){
+                        ret = otherBallBot;
+                    }
+                }
+            }
+        }
+        return ret;
     }
 }
