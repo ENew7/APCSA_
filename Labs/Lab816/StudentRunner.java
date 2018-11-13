dc5b7de981a4e3646ec9614a8f832bada3619980
ENew7 <42822049+ENew7@users.noreply.github.com>
2018-11-13 10:35:57 -0800

    no u dont

---
+++ Labs/Lab816/StudentRunner.java
@@ -0,0 +1,13 @@
+
+/**
+ *Runner Class
+ *8/16 Lab
+ */
+public class StudentRunner
+{
+   public static void main(){
+       Student s1 = new Student();
+       s1.setStudNumber(444444);
+       System.out.println(s1.getStudNumber());
+   }
+}
